bf6e7887babe90e1ff690ae49df419fba2d7890b
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-05-09 22:38:08 -0600

    Refactored code to be more readable. Added Comments. Updated image names so they display properly

--- script.js
+++ script.js
@@ -1,76 +1,110 @@
-    //$(document).ready(function(){
-        var weatherReports = {};
-        counter = 0;
-        var machoManImages = [
-            "./images/Macho-Man-Normal.png",
-            "./images/Macho-Man-Glasses.png",
-            "./images/Macho-Man-Umbrella.png",
-            "./images/Macho-Man-Scarf.png"
-        ]
+$(document).ready(function(){
+    var weatherReports = {};
+    rotationCounter = 0;
+    //array of image paths to switch between pictures of the Macho Man depending on on the weater
+    var machoManImages = [
+        "./images/Macho-Man-Normal.png",
+        "./images/Macho-Man-Glasses.png",
+        "./images/Macho-Man-Umbrella.png",
+        "./images/Macho-Man-Scarf.png"
+    ]
+    
+    getWeatherData();
+    
+    //Gets raw weather data from weather server
+    function getWeatherData(){
+        $.get('http://api.geonames.org/weatherJSON?north=41&south=39&east=-104&west=-106&username=ertyseidohl', function(response){
+            //Rotate through weather reports based on interval
+            setInterval(buildAndDisplayWeatherReport, 5000);
+            weatherReports =  response.weatherObservations;
+        })
+    }
+
+    function buildAndDisplayWeatherReport(){
+        //Take raw weather report from rotation and clean up data
+        var weatherObservation = weatherReports[rotationCounter];
+        var cloudReport = getCloudReport(weatherObservation.clouds);
+        var location = sanitizeLocation(weatherObservation.stationName);
+        var temperature = convertCelciusToFahrenheit(weatherObservation.temperature);
+
+        //Form a Macho Man Randy Savage style phrase about the weather
+        var weatherQuote = "OH YEAAAAHH! It's " + temperature +  " degrees in " + location +
+        " with " + cloudReport + " PUNK! OOOOOOHH YEAAAAH...";
+
+        displayWeatherReport(weatherQuote, cloudReport,weatherObservation.humidity, temperature)
+        updateRotation();
+    }
+
+
+    function displayWeatherReport(weatherQuote,cloudReport, humidity, temperature){
+        //display report in html
+        $('#weather-report').html(weatherQuote);
+        generateMachoMan(cloudReport,humidity, temperature );
+    }
+
+    function updateRotation(){
+        rotationCounter++;
+        //cycle back through weather reports once last report is reached
+        rotationCounter = rotationCounter % weatherReports.length;
+    }
+
+    function getCloudReport(cloudConditions){
+        //sanitize cloud report
+        if(cloudConditions == "n/a"){
+            return "no clouds";
+        }
+        return cloudConditions.toLowerCase();
+    }
+    function sanitizeLocation(location){
+        //if there is a comma, grab the part before the comma
+        location = toTitleCase(location);
+        //Splits the location at a "," and takes the piece before the ","
+        location = location.split(",")[0];
+        location = location.split("/")[0];
+        //Get rid of the word "muni" if the location contains it
+        location = location.replace("muni","");
+        return location;
+    }
+
+    function toTitleCase(str)
+    {
+        //Regular expression looks for alphanumeric characters and whitespice to apply uppercasing to 
+        return str.replace(/\w\S*/g, function(txt){
+            //Creates a simple title case string with first letter upper case and rest lowercase
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+
+    function convertCelciusToFahrenheit(tempInCelcius){ 
+        //formula for converting a celcius temperature to Fahrenheit
+        var tempInFahrenheit = Math.round((tempInCelcius * 9 / 5) + 32);
+        return tempInFahrenheit;
+    }
+
+    function generateMachoMan(clouds,humidity,temperatureInF){
+        //high temperature, no clouds ==> sun glasses
+        //low temperature ==> scarf
+        //high humidity = umbrella
+        //otherwise normal macho man
         
-        getWeatherData();
-     
-        function getWeatherData(){
-            $.get('http://api.geonames.org/weatherJSON?north=41&south=39&east=-104&west=-106&username=ertyseidohl', function(response){
-                setInterval(buildWeatherReport, 5000);
-                weatherReports =  response.weatherObservations;
-			})
+        if(temperatureInF > 70 && clouds == "no clouds"){
+            buildMachoManImageTag(machoManImages[1]);
+        }else if(temperatureInF < 32){
+            buildMachoManImageTag(machoManImages[3]);
+        }else if(humidity > 70){
+            buildMachoManImageTag(machoManImages[2]);
+        }else{
+            buildMachoManImageTag(machoManImages[0]);
         }
+    }
 
-        function buildWeatherReport(){
+    function buildMachoManImageTag(machoManImagePath){
+        $('#macho-man').empty().append(buildImageTag(machoManImagePath, "Macho Weather Man Randy Savage"));
+    }
 
-            var weatherObservation = weatherReports[counter];
-            var cloudReport = getCloudReport(weatherObservation.clouds);
-            var location = parseLocation();
-
-            var phrase = "OH YEAAAAHH! It's " + weatherObservation.temperature +  " degrees in " + parseLocation(weatherObservation.stationName) +
-            " with " + cloudReport + " PUNK! OOOOOOHH YEAAAAH...";
-
-            //display report
-            $('#weather-report').html(phrase);
-            generateMachoMan(cloudReport,weatherObservation.humidity, weatherObservation.temperature );
-            counter++;
-            //cycle back through weather reports once last report is reached
-            counter = counter % weatherReports.length;
-           
-        }
-
-        function getCloudReport(cloudConditions){
-            if(cloudConditions == "n/a"){
-                return "no clouds";
-            }
-            return cloudConditions;
-        }
-        function parseLocation(location){
-            //TODO: 
-            //if there is a comma, grab the part before the comma
-            //If no comma, just grab the first word
-            return location;
-
-        }
-
-        function convertCelciusToFahrenheit(tempInCelcius){
-            
-        }
-
-        function generateMachoMan(clouds,humidity,temperature){
-            //high temperature, no clouds ==> sun glasses
-            //low temperature ==> scarf
-            //high humidity = umbrella
-            //otherwise normal macho man
-            
-            if(temperature > 70 && clouds == "no clouds"){
-                $('#macho-man').empty().append(buildImageTag(machoManImages[1]));
-            }else if(temperature < 32){
-                $('#macho-man').empty().append(buildImageTag(machoManImages[3]));
-            }else if(humidity > 70){
-                $('#macho-man').empty().append(buildImageTag(machoManImages[2]));
-            }else{
-                $('#macho-man').empty().append(buildImageTag(machoManImages[0]));
-            }
-        }
-
-        function buildImageTag(imagePath){
-            var imageTag = "<img src='" + imagePath + "'/>";
-            return imageTag;
-        }
+    //Builds a generic image tag with a path and alt attribute
+    function buildImageTag(imagePath, altText){
+        var imageTag = "<img src='" + imagePath + "' alt='" + altText + "'/>";
+        return imageTag;
+    }
+});
